6c33cf244481161fd2c21c3a60fbf29e17102957
GDJ29 <GDJ29@DESKTOP-5H1FDF5>
2020-11-04 15:24:22 +0900

    [박용호]cashbook

---
+++ cash/src/main/java/kr/co/gdu/cash/controller/CashbookController.java
@@ -0,0 +1,108 @@
+package kr.co.gdu.cash.controller;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import kr.co.gdu.cash.service.CashbookService;
+import kr.co.gdu.cash.service.CategoryService;
+import kr.co.gdu.cash.service.IndexService;
+import kr.co.gdu.cash.vo.Cashbook;
+import kr.co.gdu.cash.vo.Category;
+import kr.co.gdu.cash.vo.Notice;
+
+@Controller
+public class CashbookController {
+	@Autowired
+	private CashbookService cashbookService;
+	@Autowired CategoryService categoryService;
+	
+	@GetMapping(value="cashbookByMonth")
+	public String cashbookByMonth(Model model,
+			@RequestParam(name = "currentYear", defaultValue = "-1") int currentYear,
+			@RequestParam(name = "currentMonth", defaultValue = "-1") int currentMonth) {
+		
+		// 1. 요청분석
+		Calendar currentDay = Calendar.getInstance(); // 2020년 11월 2일
+		//currentYear 넘어오고, currentMonth 넘어오면
+		if(currentYear !=-1 || currentMonth != -1) {
+			if(currentMonth==0) {
+				currentYear-=1;
+				currentMonth =12;
+			}
+			if(currentMonth==13) {
+				currentYear+=1;
+				currentMonth=1;
+			}
+			currentDay.set(Calendar.YEAR, currentYear);
+			currentDay.set(Calendar.MONTH, currentMonth -1);
+		}
+		
+		currentDay.set(Calendar.DATE, 1); //2020년 11월 1일
+		currentYear = currentDay.get(Calendar.YEAR); 
+		currentMonth = currentDay.get(Calendar.MONTH) +1; //월
+		int lastDay = currentDay.getActualMaximum(Calendar.DATE);	
+		int firstDayOfWeek = currentDay.get(Calendar.DAY_OF_WEEK);
+		//--------------------------------------------------------------------------------------------
+		int sumOut= cashbookService.getselectSumCashbookPriceByInOut("수입", currentYear, currentMonth);
+		int sumIn =	cashbookService.getselectSumCashbookPriceByInOut("지출", currentYear, currentMonth);
+		//--------------------------------------------------------------------------------------------
+		List<Map<String, Object>> cashList = cashbookService.getCashListByMonth(currentYear, currentMonth);
+		//--------------------------------------------------------------------------------------------
+		
+		// 2. 뷰 모델 추가
+		model.addAttribute("currentYear", currentYear); // 년
+		model.addAttribute("currentMonth", currentMonth); // 월
+		model.addAttribute("lastDay", lastDay); // 마지막 일
+		model.addAttribute("firstDayOfWeek", firstDayOfWeek);
+		
+		model.addAttribute("sumOut", sumOut);
+		model.addAttribute("sumIn", sumIn);
+		
+		model.addAttribute("cashList", cashList);
+		
+		return "cashbookByMonth";
+	}
+	
+	@GetMapping("/cashbookByDay")
+	public String cashbookByDay(Model model,
+			@RequestParam(name = "currentYear", required = true) int currentYear,
+			@RequestParam(name = "currentMonth", required = true) int currentMonth,
+			@RequestParam(name = "currentDay", required = true) int currentDay) {
+		
+		List<Cashbook> cashbookList = cashbookService.getCashbookListByDay(currentYear, currentMonth, currentDay);
+		
+		model.addAttribute("cashbookList", cashbookList);
+
+		
+		
+		return "cashbookByDay";
+	}
+	
+	@GetMapping("/addCashbook")
+	public String addCashbook(Model model,
+			@RequestParam(name = "currentYear", required = true) int currentYear,
+			@RequestParam(name = "currentMonth", required = true) int currentMonth,
+			@RequestParam(name = "currentDay", required = true) int currentDay) {
+		
+		List<Category> categoryList = categoryService.getCategoryList();
+		model.addAttribute("categoryList", categoryList);
+		
+		return "addCashbook";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ cash/src/main/java/kr/co/gdu/cash/mapper/CashbookMapper.java
@@ -0,0 +1,16 @@
+package kr.co.gdu.cash.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.co.gdu.cash.vo.Cashbook;
+
+@Mapper
+public interface CashbookMapper {
+	List<Map<String, Object>> selectCashInOutList();
+	int selectSumCashbookPriceByInOut(Map<String, Object> map);
+	List<Map<String, Object>> selectCashListByMonth(Map<String, Object> map);
+	List<Cashbook> selectCashbookListByDay(Map<String, Object> map);
+}
